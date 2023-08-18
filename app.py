ed706e8d52b82fb1d0015d0e6089d7e062161f53
Roberto Saldaña <1113.rsaldaa@gruporeal.ad>
2023-08-18 09:00:33 -0600

    argumentos y id

--- app.py
+++ app.py
@@ -1,13 +1,52 @@
 from flask import Flask
-from flask_restx import Resource, Api
+from flask_restx import Resource, Api, fields, reqparse, inputs
+from persona import Persona
+
+listPersonas = [
+    Persona(1, 'Pedro Andrés', 'Vega Stalling', False),
+    Persona(2, 'José Roberto', 'Saldaña Arrazola', True),
+    Persona(3, 'Selvin Fernando', 'Ac Cucul', False),
+    Persona(4, 'Erick Daniel', 'Poron Muñoz', True),
+    Persona(5, 'Erick Fernando', 'Canto Boton', False),
+    Persona(6, 'Elisa María', 'Jauregui', True),
+    Persona(7, 'Ana Beatruiz', 'Obiols', True),
+    Persona(8, 'Jonathan', 'Monroy', False),
+]
+
+def find(id: int):
+    output: Persona = None
+    for persona in listPersonas:
+        if persona.codigo == id:
+            output = persona
+    return output
 
 app = Flask(__name__)
 api = Api(app)
 
+personaModel = api.model('PersonaModel', {
+    'codigo': fields.Integer,
+    'nombres': fields.String,
+    'apellidos': fields.String,
+    'tieneVisa': fields.Boolean
+})
+
+
 @api.route('/personas')
 class PersonasResource(Resource):
+    
+    parser = reqparse.RequestParser()
+    parser.add_argument('tieneVisa', type=inputs.boolean)
+    @api.marshal_list_with(personaModel)
     def get(self):
-        return {'post': 'hello'}
+        args = self.parser.parse_args()
+        output=[]
+        if args['tieneVisa']!=None:
+            for i in listPersonas:
+                if i.tieneVisa==args['tieneVisa']:
+                    output.append(i)
+        else:
+            return listPersonas
+        return output
     
     def post(self):
         return {'post' : 'world'}
@@ -15,11 +54,14 @@ class PersonasResource(Resource):
 
 @api.route('/personas/<int:id>')
 class PersonaResource(Resource):
+
+    @api.marshal_with(personaModel)
     def get(self, id):
-        return{'get':'get por id='+str(id)}
+        return find(id)
     
     def put(self, id):
-        return{'put':'put por id='+str(id)}
+        persona = find(id)
+        return persona
 
 
 @api.route('/personas/pg')
---
+++ persona.py
@@ -0,0 +1,7 @@
+class Persona():
+    
+    def __init__(self, codigo: int, nombres: str, apellidos: str, tieneVisa: bool):
+        self.codigo = codigo
+        self.nombres = nombres
+        self.apellidos = apellidos
+        self.tieneVisa = tieneVisa
