ed58953c1876c39cafd0ebc1a84fc9528990c501
Pedro Vega <csa@gruporealcaruz.com>
2023-08-23 09:36:51 -0600

    se agrega la tabla de empresas y la llave foranea en personas

--- database/empresas.py
+++ database/empresas.py
@@ -3,7 +3,7 @@ from database import db
 class Empresas(db.Model):
     __tablename__ = 'empresas'
 
-    id: int = db.Column('id', db.SmallInteger, nullable=False, primary_key=True, autoincrement=True,)
+    codEmpresa: int = db.Column('cod_empresa', db.SmallInteger, nullable=False, primary_key=True, autoincrement=True)
     nombre: str = db.Column('nombre',  db.String(100), nullable=False)
     direccion: str = db.Column('direccion',  db.String(100), nullable=True)
     telefono: str = db.Column('telefono', db.String(12), nullable=False)
